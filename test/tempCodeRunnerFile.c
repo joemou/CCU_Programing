dd2879328591b1f5d437986fb961667254c972d7
joemou <60748971+joemou@users.noreply.github.com>
2022-12-04 21:50:08 +0800

    hw

--- test/tempCodeRunnerFile.c
+++ test/tempCodeRunnerFile.c
@@ -9,35 +9,30 @@ Open/Close boosters:
   [2] Price booster (now close)
   [3] Area  booster (now close)
   [4] Finish
-Enter the number(s): 2
-Open/Close boosters:
+Enter the number(s): Open/Close boosters:
   [1] Speed booster (now close)
   [2] Price booster (now open)
   [3] Area  booster (now close)
   [4] Finish
-Enter the number(s): 1
-Open/Close boosters:
+Enter the number(s): Open/Close boosters:
   [1] Speed booster (now open)
   [2] Price booster (now open)
   [3] Area  booster (now close)
   [4] Finish
-Enter the number(s): 4
-Actions you can take for each area:
+Enter the number(s): Actions you can take for each area:
   [1] Sell the hotdogs
   [2] Improve your cooking speed
-      (- $50, - $100, - $200, - $400 for next four upgrades) 
+      (- $50, - $100, - $200, - $400 for next four upgrades)
   [3] Improve your hotdog flavor
       (- $100, - $200, - $400, - $800 for next four upgrades)
-Enter the number(s): 1 1 2 1
-Well done, you earn $4320 today.
+Enter the number(s): Well done, you earn $4320 today.
 Which result of the area you want to check?
   [1] Area 1
   [2] Area 2
   [3] Area 3
   [4] Area 4
   [5] Done
-Enter the number(s): 3
-You glimpse the secret of wind.
+Enter the number(s): You glimpse the secret of wind.
 Your hands can move faster now.
 Which result of the area you want to check?
   [1] Area 1
@@ -45,8 +40,7 @@ Which result of the area you want to check?
   [3] Area 3
   [4] Area 4
   [5] Done
-Enter the number(s): 4
-You make 24 hotdogs here!
+Enter the number(s): You make 24 hotdogs here!
 You earn $1440!
 Which result of the area you want to check?
   [1] Area 1
@@ -54,8 +48,7 @@ Which result of the area you want to check?
   [3] Area 3
   [4] Area 4
   [5] Done
-Enter the number(s): 4
-You make 24 hotdogs here!
+Enter the number(s): You make 24 hotdogs here!
 You earn $1440!
 Which result of the area you want to check?
   [1] Area 1
@@ -63,28 +56,24 @@ Which result of the area you want to check?
   [3] Area 3
   [4] Area 4
   [5] Done
-Enter the number(s): 84
-Invalid input!!!!
+Enter the number(s): Invalid input!!!!
 Which result of the area you want to check?
   [1] Area 1
   [2] Area 2
   [3] Area 3
   [4] Area 4
   [5] Done
-Enter the number(s): 41
-Invalid input!!!!
+Enter the number(s): Invalid input!!!!
 Which result of the area you want to check?
   [1] Area 1
   [2] Area 2
   [3] Area 3
   [4] Area 4
   [5] Done
-Enter the number(s): 5
-Do you want to continue or exit?
+Enter the number(s): Do you want to continue or exit?
   [1] Continue
   [2] Exit
-Enter the number(s): 1
-Chop chop, It's dawn.
+Enter the number(s): Chop chop, It's dawn.
 You have 4370 dollars.
 You need 14 minutes to make a hotdog.
 The price of a hotdog is $30.
@@ -94,75 +83,63 @@ Open/Close boosters:
   [2] Price booster (now open)
   [3] Area  booster (now close)
   [4] Finish
-Enter the number(s): 1
-Open/Close boosters:
+Enter the number(s): Open/Close boosters:
   [1] Speed booster (now close)
   [2] Price booster (now open)
   [3] Area  booster (now close)
   [4] Finish
-Enter the number(s): 2
-Open/Close boosters:
+Enter the number(s): Open/Close boosters:
   [1] Speed booster (now close)
   [2] Price booster (now close)
   [3] Area  booster (now close)
   [4] Finish
-Enter the number(s): 3
-Open/Close boosters:
+Enter the number(s): Open/Close boosters:
   [1] Speed booster (now close)
   [2] Price booster (now close)
   [3] Area  booster (now open)
   [4] Finish
-Enter the number(s): 4
-Actions you can take for each area:
+Enter the number(s): Actions you can take for each area:
   [1] Sell the hotdogs
   [2] Improve your cooking speed
       (- $100, - $200, - $400, - $800 for next four upgrades)
   [3] Improve your hotdog flavor
       (- $100, - $200, - $400, - $800 for next four upgrades)
-Enter the number(s): 98 78 15 1
-Invalid input!!!!
+Enter the number(s): Invalid input!!!!
 Actions you can take at Area 1:
   [1] Sell the hotdogs
   [2] Improve your cooking speed
   [3] Improve your hotdog flavor
-Enter the number(s): 2
-Invalid input!!!!
+Enter the number(s): Invalid input!!!!
 Actions you can take at Area 2:
   [1] Sell the hotdogs
   [2] Improve your cooking speed
   [3] Improve your hotdog flavor
-Enter the number(s): 4
-Invalid input!!!!
+Enter the number(s): Invalid input!!!!
 Actions you can take at Area 2:
   [1] Sell the hotdogs
   [2] Improve your cooking speed
   [3] Improve your hotdog flavor
-Enter the number(s): 6
-Invalid input!!!!
+Enter the number(s): Invalid input!!!!
 Actions you can take at Area 2:
   [1] Sell the hotdogs
   [2] Improve your cooking speed
   [3] Improve your hotdog flavor
-Enter the number(s): 1
-Invalid input!!!!
+Enter the number(s): Invalid input!!!!
 Actions you can take at Area 3:
   [1] Sell the hotdogs
   [2] Improve your cooking speed
   [3] Improve your hotdog flavor
-Enter the number(s): 27
-Invalid input!!!!
+Enter the number(s): Invalid input!!!!
 Actions you can take at Area 3:
   [1] Sell the hotdogs
   [2] Improve your cooking speed
   [3] Improve your hotdog flavor
-Enter the number(s): 63
-Invalid input!!!!
+Enter the number(s): Invalid input!!!!
 Actions you can take at Area 3:
   [1] Sell the hotdogs
   [2] Improve your cooking speed
   [3] Improve your hotdog flavor
-Enter the number(s): 1
-Well done, you earn $1560 today.
+Enter the number(s): Well done, you earn $1560 today.
 Which result of the area you want to check?
   [1] Area 1
   [2] Area 2
@@ -170,8 +147,7 @@ Which result of the area you want to check?
   [4] Area 4
   [5] Area 5
   [6] Done
-Enter the number(s): 53
-Invalid input!!!!
+Enter the number(s): Invalid input!!!!
 Which result of the area you want to check?
   [1] Area 1
   [2] Area 2
@@ -179,8 +155,7 @@ Which result of the area you want to check?
   [4] Area 4
   [5] Area 5
   [6] Done
-Enter the number(s): 1
-You glimpse the secret of wind.
+Enter the number(s): You glimpse the secret of wind.
 Your hands can move faster now.
 Which result of the area you want to check?
   [1] Area 1
@@ -189,8 +164,7 @@ Which result of the area you want to check?
   [4] Area 4
   [5] Area 5
   [6] Done
-Enter the number(s): 2
-You make 13 hotdogs here!
+Enter the number(s): You make 13 hotdogs here!
 You earn $390!
 Which result of the area you want to check?
   [1] Area 1
@@ -199,8 +173,7 @@ Which result of the area you want to check?
   [4] Area 4
   [5] Area 5
   [6] Done
-Enter the number(s): 3
-You make 13 hotdogs here!
+Enter the number(s): You make 13 hotdogs here!
 You earn $390!
 Which result of the area you want to check?
   [1] Area 1
@@ -209,8 +182,7 @@ Which result of the area you want to check?
   [4] Area 4
   [5] Area 5
   [6] Done
-Enter the number(s): 4
-You make 13 hotdogs here!
+Enter the number(s): You make 13 hotdogs here!
 You earn $390!
 Which result of the area you want to check?
   [1] Area 1
@@ -219,8 +191,7 @@ Which result of the area you want to check?
   [4] Area 4
   [5] Area 5
   [6] Done
-Enter the number(s): 5
-You make 13 hotdogs here!
+Enter the number(s): You make 13 hotdogs here!
 You earn $390!
 Which result of the area you want to check?
   [1] Area 1
@@ -229,8 +200,7 @@ Which result of the area you want to check?
   [4] Area 4
   [5] Area 5
   [6] Done
-Enter the number(s): 6
-Do you want to continue or exit?
+Enter the number(s): Do you want to continue or exit?
   [1] Continue
   [2] Exit
-Enter the number(s): 2
+Enter the number(s): We will miss you. Bye!
