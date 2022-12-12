aba0df27fc7d773aeabf7db0924e3246987ac29d
joemou <60748971+joemou@users.noreply.github.com>
2022-12-12 15:48:07 +0800

    hw

--- 111-1Final project/Final_Project.c
+++ 111-1Final project/Final_Project.c
@@ -5,8 +5,8 @@
 void selling_hotdog(int *money, int *price,int *start, int *booster_owned);
 void lottery(int *money, int *price, int *booster_slot, int *booster_owned);
 
-
-
+void map_control(int *money,int *booster_owned);
+void map_print(int i, int j,int *b,int *money,int *booster_owned);
 
 int main(){
 int money = 100, price = 30;
@@ -262,27 +262,31 @@ printf("Welcome, young boss!\n");
                         printf("Enter the number(s): ");
                         scanf("%d", &check_selection);
                      if(check_selection>6||check_selection<1){
-                            printf("Invalid input!!!!\n");
-                            continue;
+                        printf("Invalid input!!!!\n");
+                        continue;
                      }   
                      if(result[check_selection]==1){
                         
-                            printf("Can't you tell how poor you are?\n");
-				            printf("Go vending your hotdogs instead of thinking about self-improvement!\n");
+                        printf("Can't you tell how poor you are?\n");
+				        printf("Go vending your hotdogs instead of thinking about self-improvement!\n");
+                        map_control(&*money, booster_owned);
                         
                      }    
                      else if(result[check_selection]==2){
-                            printf("Do you want to travel through time?\n");
-			                printf("GO WORK!!\n");
+                        printf("Do you want to travel through time?\n");
+			            printf("GO WORK!!\n");
+                        map_control(&*money, booster_owned);
                         
                      }
                       else if (result[check_selection] == 4){
                         printf("You glimpse the secret of wind.\n");
 				        printf("Your hands can move faster now.\n");
+                        map_control(&*money, booster_owned);
                      }
                      else if (result[check_selection] == 5){
                         printf("You feel the soul of the ingredients.\n");
 				        printf("Your hotdogs are more appetizing now.\n");
+                        map_control(&*money, booster_owned);
                         }
                      if(result[check_selection]<=3){
                         if(check_selection==5&&booster[2]==0)
@@ -291,7 +295,7 @@ printf("Welcome, young boss!\n");
                             break;
                         printf("You make %d hotdogs here!\n", hotdog_make[check_selection]);
 				        printf("You earn $%d!\n", money_make[check_selection]);
-                        
+                        map_control(&*money, booster_owned);
                      }
                      }
 
@@ -501,4 +505,150 @@ time_t t;
 
     }
 
+}
+
+void map_control(int *money,int *booster_owned){
+    int start = 1;
+    int i=0;
+    int x = 0, y = 0;
+    time_t t;
+    int  b[4];//money and booster coordinate
+    srand(time(&t));   
+    
+    //generate random number for coordinate and prevent their coordinate to overlaping
+    do{
+
+        for (int l = 0; l < 4;l++){
+            b[l] = rand() % 8;
+        }
+    
+    } while ((b[0]==b[1]==b[2]==b[3]));
+
+        map_print(y, x, b,money,booster_owned);
+
+    while(start!=0){
+     printf("\nPlease input (1: up, 2: down, 3: left, and 4: right 5: finished) to indicate move action.\n25$ for each action.\nENTER:");
+
+     scanf("%d", &i);
+
+     if(i==1){
+
+    		y=y-1;//up
+            if(y!=-1){
+                map_print(y,x,b,money,booster_owned);
+                money -= 25;
+            }
+
+            else{ 
+                printf("\n\nyou touch the board\nplease choose again\n");
+                y = y + 1;
+            }
+            
+
+        }
+	 else if(i==2){
+			y=y+1;//down
+
+            if(y!=8){
+                map_print(y,x,b,money,booster_owned);
+                money -= 25;
+            }
+
+            else{ 
+                printf("\n\nyou touch the board\nplease choose again\n");
+                y = y -1;
+            }
+		}
+	 else if(i==3){
+
+			x=x-1;//left
+
+            if(x!=-1){
+                map_print(y,x,b,money,booster_owned);
+                money -= 25;
+            }
+
+            else{ 
+                printf("\n\nyou touch the board\nplease choose again\n");
+                x = x + 1;
+            }
+		}
+	 else if(i==4){
+
+			x=x+1;//right
+
+            if(x!=8){
+                map_print(y,x,b,money,booster_owned);
+                money -= 25;
+            }
+
+            else{ 
+                printf("\n\nyou touch the board\nplease choose again\n");
+                x = x - 1;
+            }
+		}
+     else if(i==5){
+            start=0;
+     }
+	 
+
+    }
+}
+
+void map_print(int i, int j,int *b,int *money,int *booster_owned){
+    
+    int a[9][9];
+    int x, y;
+    time_t t;
+    srand(time(&t)); 
+
+     
+
+    for (x = 0; x < 8;x++){
+     
+     for (y = 0; y < 8;y++){
+            a[x][y]=0; //determine the dot is 0
+    }
+    }
+
+
+    a[b[0]][b[1]] = 2;//determine the place of money award is 2
+    a[b[2]][b[3]] = 3;//determine the plcae of booster award is 3
+    a[i][j] = 1;//determine the person place. person is 1.
+        
+
+
+    
+    
+    for (x = 0; x < 8;x++){
+        printf("\n");
+      for (y = 0; y < 8;y++){
+        if(a[x][y]==0)
+            printf(". ");
+        else if(a[x][y]==1)
+            printf("P ");
+        else if(a[x][y]==2)
+            printf("M ");
+        else if(a[x][y]==3)
+            printf("B ");
+
+     }
+    }
+    printf("\n");
+    printf("%d %d %d %d $%d\n", b[0], b[1], i, j,*money);
+     
+     
+     if ((b[0] == i)&&(b[1]== j)){
+        printf("Fortune!Fortune! You earn %d !!!!\n", (b[0] + 1) * (b[1] + 1) * 10);
+        *money += (b[0] + 1) * (b[1] + 1) * 10;
+        b[0] = b[1] = -1; //let it outside the board
+        }
+
+     if((b[2]==i)&&(b[3]==j)){
+        printf("Congratulation, you earn a booster\n");
+        booster_owned[rand() % 3] += 1;
+        printf("You owned speed %d price %d area %d\n",booster_owned[0],booster_owned[1],booster_owned[2]);
+        b[2] = b[3] = -1; //let it outside the board
+    }
+    
 }
--- 111-1Final project/map_system.c
+++ 111-1Final project/map_system.c
@@ -2,92 +2,150 @@
 #include <stdlib.h>
 #include <time.h>
 
-int map(int i, int j);
-int transfrom(int input);
+void map(int i, int j,int *b,int *money,int *booster_owned);
 int main(){
     int start = 1;
     int i=0;
     int x = 0, y = 0;
     int money = 1000000;
+    int booster_owned[3];
+    time_t t;
+    int  b[4];//money and booster coordinate
+    srand(time(&t));   
+    do{
 
-    map(x,y);
+        for (int l = 0; l < 4;l++){
+            b[l] = rand() % 8;
+        }
+    
+    } while ((b[0]==b[1]==b[2]==b[3])); //generate random number for coordinate and prevent their coordinate to overlaping
+
+        map(x, y, b,&money,booster_owned);
+
     while(start!=0){
-     printf("\nPlease input (1: up, 2: down, 3: left, and 4: right 5: finished) to indicate move action.\n25$ for eachaction.");
+     printf("\nPlease input (1: up, 2: down, 3: left, and 4: right 5: finished) to indicate move action.\n25$ for each action.\nENTER:");
 
      scanf("%d", &i);
-	 if(y==0 || x==0){
-
-			printf("\n\nyou touch the board\nplease choose again\n");
-
-			continue; 
-		}
-	 if(y==9 || x==9){
-
-			printf("\n\nyou touch the board\nplease choose again\n");
-			continue;
-		}    
 
      if(i==1){
-    		map(x,x);
-    		y=y-1;
-            money -= 25;
+
+    		y=y-1;//up
+            if(y!=-1){
+                map(y,x,b,&money,booster_owned);
+                money -= 25;
+            }
+
+            else{ 
+                printf("\n\nyou touch the board\nplease choose again\n");
+                y = y + 1;
+            }
+            
+
         }
 	 else if(i==2){
-			map(y+1,x);
-			y=y+1;
-            money -= 25;
+			y=y+1;//down
+
+            if(y!=8){
+                map(y,x,b,&money,booster_owned);
+                money -= 25;
+            }
+
+            else{ 
+                printf("\n\nyou touch the board\nplease choose again\n");
+                y = y -1;
+            }
 		}
 	 else if(i==3){
-			map(y,x-1);
-			x=x-1;
-            money -= 25;
+
+			x=x-1;//left
+
+            if(x!=-1){
+                map(y,x,b,&money,booster_owned);
+                money -= 25;
+            }
+
+            else{ 
+                printf("\n\nyou touch the board\nplease choose again\n");
+                x = x + 1;
+            }
 		}
 	 else if(i==4){
-			map(y,x+1);
-			x=x+1;
-            money -= 25;
+
+			x=x+1;//right
+
+            if(x!=8){
+                map(y,x,b,&money,booster_owned);
+                money -= 25;
+            }
+
+            else{ 
+                printf("\n\nyou touch the board\nplease choose again\n");
+                x = x - 1;
+            }
 		}
      else if(i==5){
-            break;
+            start=0;
      }
+	 
 
     }
 }
-int map(int i, int j){
+
+
+
+void map(int i, int j,int *b,int *money,int *booster_owned){
     int a[9][9];
     int x, y;
     time_t t;
-    srand(time(&t));
+    srand(time(&t)); 
+
+     
 
     for (x = 0; x < 8;x++){
      
      for (y = 0; y < 8;y++){
-            a[x][y]=0; //determine the dot
-     }
+            a[x][y]=0; //determine the dot is 0
+    }
     }
 
+
+    a[b[0]][b[1]] = 2;//determine the place of money award is 2
+    a[b[2]][b[3]] = 3;//determine the plcae of booster award is 3
     a[i][j] = 1;//determine the person place. person is 1.
-    a[rand() % 9][rand() % 9] = 2;//determine the place of money award
-    a[rand() % 9][rand() % 9] = 3;//determine the plcae of booster award
+        
+
+
     
     
     for (x = 0; x < 8;x++){
         printf("\n");
-     for (y = 0; y < 8;y++){
-     if(a[x][y]==0)
-         printf(". ");
-     
-     else if(a[x][y]==1)
-         printf("P ");
-     
-     else if(a[x][y]==2)
-         printf("M ");
-     
-     else if(a[x][y]==3)
-         printf("B ");
+      for (y = 0; y < 8;y++){
+        if(a[x][y]==0)
+            printf(". ");
+        else if(a[x][y]==1)
+            printf("P ");
+        else if(a[x][y]==2)
+            printf("M ");
+        else if(a[x][y]==3)
+            printf("B ");
+
      }
-
     }
+    printf("\n");
+    printf("%d %d %d %d\n", b[0], b[1], i, j);
+     
+     
+     if ((b[0] == i)&&(b[1]== j)){
+        printf("Fortune!Fortune! You earn %d !!!!\n", (b[0] + 1) * (b[1] + 1) * 10);
+        *money += (b[0] + 1) * (b[1] + 1) * 10;
+        b[0] = b[1] = -1; //let it outside the board
+        }
 
-    return 0;
+     if((b[2]==i)&&(b[3]==j)){
+        printf("Congratulation, you earn a booster\n");
+        booster_owned[rand() % 3] += 1;
+        printf("You owned speed %d price %d area %d\n",booster_owned[0],booster_owned[1],booster_owned[2]);
+        b[2] = b[3] = -1; //let it outside the board
+    }
+    
 }
