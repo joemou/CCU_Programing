43c4d581f439b6c99d13e482649733b628d5629c
joemou <60748971+joemou@users.noreply.github.com>
2023-03-19 18:58:49 +0800

    practice

--- school/111-2/ex/EX3/sourceCode/basic.h
+++ school/111-2/ex/EX3/sourceCode/basic.h
@@ -1,21 +1,28 @@
 #ifndef BASIC_H
 #define BASIC_H
-
 #include <stdio.h>      // For standard I/O functions
 #include <stdlib.h>     // For memory allocation functions
 #include <string.h>     // For string manipulation functions
-#include <stdbool.h>    // For boolean data type
+#include <ctype.h>
 
-// External variables
-extern int global_int;
-extern char global_char;
+int isAddOrMinus(char *op);
 
-// Identifiers
+#define MAXBUF 10000
 #define TRUE 1
 #define FALSE 0
-typedef int Bool;
+#define SAME 0
+#define LOWER -1
+#define HIGHER 1
+#define Rev 2
+#define Nor 1
+#define TooMany 1
+#define TooLess -1
+#define INFIX 1
+#define PREFIX 2
+#define POSTFIX 3
+#define Unknown 2
+#define Duplicate 3
 
-#define PI 3.14159
-#define MAX(a,b) ((a) > (b) ? (a) : (b))
+
 
 #endif /* BASIC_H */
