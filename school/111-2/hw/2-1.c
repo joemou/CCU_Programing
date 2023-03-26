48b6510505caa4516a22291c3e3e34c67a8d90a8
joemou <60748971+joemou@users.noreply.github.com>
2023-03-27 00:48:22 +0800

    practice

--- school/111-2/hw/2-1.c
+++ school/111-2/hw/2-1.c
@@ -2,6 +2,64 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
+typedef struct course
 {
+    char *subject;
+    struct node *prev;
+    struct node *next;
+} node;
+
+node *getnode(char *str)
+{
+    node *newnode = (node *)malloc(sizeof(node));
+    newnode->subject = strdup(str);
+    newnode->prev = NULL;
+    newnode->next = NULL;
+    return newnode;
+}
+
+node *create(int *num, int *next, int *prev, char *str[], node *head, int how_many)
+{
+
+    int at = 0;
+
+    while (how_many--)
+    {
+        node *temp = head;
+        if (head == NULL)
+        {
+            head = getnode(str[at]);
+        }
+        else
+        {
+            for (int i = 0; i < how_many; i++)
+            {
+                if (num[at] == next[i])
+                {
+                    temp->next = getnode(str[i]);
+                }
+            }
+        }
+
+        at++;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int num1[100], num2[100], num3[100];
+    char *str[100];
+    node *head = NULL;
+    int k = 0;
+    for (int i = 1; i < argc; i += 4)
+    {
+        num1[k] = atoi(argv[i]);
+        num2[k] = atoi(argv[i + 1]);
+        num3[k] = atoi(argv[i + 2]);
+        str[k] = argv[i + 3];
+        k++;
+    }
+
+    head = create(num1, num2, num3, str, head, k);
+    return 0;
 }
