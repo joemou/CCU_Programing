4358e05f0cb240580cf63ae76db1a5ee17b8f543
joemou <60748971+joemou@users.noreply.github.com>
2023-03-27 01:01:09 +0800

    practice

--- school/111-2/hw/2-1.c
+++ school/111-2/hw/2-1.c
@@ -5,38 +5,40 @@
 typedef struct course
 {
     char *subject;
-    struct node *prev;
-    struct node *next;
-} node;
+    struct course *prev;
+    struct course *next;
+} course;
 
-node *getnode(char *str)
+
+course *getnode(char *str)
 {
-    node *newnode = (node *)malloc(sizeof(node));
+    course *newnode = (course *)malloc(sizeof(course));
     newnode->subject = strdup(str);
     newnode->prev = NULL;
     newnode->next = NULL;
     return newnode;
 }
 
-node *create(int *num, int *next, int *prev, char *str[], node *head, int how_many)
+
+course *create(int *num, int *next, int *prev, char *str[], course *head, int how_many)
 {
 
     int at = 0;
 
     while (how_many--)
     {
-        node *temp = head;
+        course *temp = head;
         if (head == NULL)
         {
             head = getnode(str[at]);
         }
         else
         {
-            for (int i = 0; i < how_many; i++)
+            for (int j = 0; j < how_many; j++)
             {
-                if (num[at] == next[i])
+                if (num[at] == next[j])
                 {
-                    temp->next = getnode(str[i]);
+                    temp->next = getnode(str[j]);
                 }
             }
         }
@@ -49,7 +51,7 @@ int main(int argc, char *argv[])
 {
     int num1[100], num2[100], num3[100];
     char *str[100];
-    node *head = NULL;
+    course *head = NULL;
     int k = 0;
     for (int i = 1; i < argc; i += 4)
     {
