b99193307e64e78c429f63ad5a28505eca450937
joemou <60748971+joemou@users.noreply.github.com>
2023-08-22 12:40:41 +0800

    update

--- school/final/no_fishing.c
+++ school/final/no_fishing.c
@@ -1,60 +1,111 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define MAX_MESSAGES 100
-#define MAX_NAME_LENGTH 20
-#define MAX_MESSAGE_LENGTH 100
+#define MAX_MESSAGES 5000
+#define MAX_NAME_LENGTH 5000
+#define MAX_MESSAGE_LENGTH 5000
 
 typedef struct {
     char name[MAX_NAME_LENGTH];
     char message[MAX_MESSAGE_LENGTH];
-    int warnings;
 } ChatMessage;
+
+// Node structure for the map
+typedef struct {
+    char* key;
+    int value;
+} MapNode;
+
+// Function to create a new map node
+MapNode* createNode(const char* key, int value) {
+    MapNode* node = (MapNode*)malloc(sizeof(MapNode));
+    node->key = strdup(key);
+    node->value = value;
+    return node;
+}
+
+
+
+// Function to get the value associated with a key in the map
+int getValue(MapNode** map, const char* key) {
+    for (int i = 0; map[i] != NULL; i++) {
+        if (strcmp(map[i]->key, key) == 0) {
+            return map[i]->value;
+        }
+    }
+    return -1;  // Key not found
+}
+
+void changeValue(MapNode** map, const char* key, int newValue) {
+    for (int i = 0; map[i] != NULL; i++) {
+        if (strcmp(map[i]->key, key) == 0) {
+            map[i]->value = newValue;
+            return;
+        }
+    }
+}
 
 int main() {
     int n;
     char bannedWord[MAX_MESSAGE_LENGTH];
     ChatMessage messages[MAX_MESSAGES];
 
+    MapNode* map[MAX_MESSAGES];
+    int mapSize = 0;
+
     scanf("%d %s", &n, bannedWord);
-    getchar(); // Consume the newline character after reading bannedWord
+    getchar();  // Consume the newline character after reading bannedWord
 
     for (int i = 0; i < n; i++) {
         char line[MAX_NAME_LENGTH + MAX_MESSAGE_LENGTH + 3];
         fgets(line, sizeof(line), stdin);
-        line[strcspn(line, "\n")] = '\0'; // Remove the trailing newline character
+        line[strcspn(line, "\n")] = '\0';  // Remove the trailing newline character
 
-        char *name = strtok(line, ":");
-        char *message = strtok(NULL, "\0");
+        char* name = strtok(line, ":");
+        char* message = strtok(NULL, "\0");
         if (name && message) {
             strcpy(messages[i].name, name);
             strcpy(messages[i].message, message);
-            messages[i].warnings = 0;
-        } else {
-            printf("Invalid input format for message %d\n", i + 1);
-            return 1;
-        }
+
+            // Check if the name is already in the map
+            int existingIndex = -1;
+            for (int j = 0; j < mapSize; j++) {
+                if (strcmp(map[j]->key, name) == 0) {
+                    existingIndex = j;
+                    break;
+                }
+            }
+
+            if (existingIndex == -1) {
+                // Name not found in the map, create a new node
+                map[mapSize] = createNode(name, 0);
+                mapSize++;
+            }
+        } 
     }
 
     for (int i = 0; i < n; i++) {
         // Check if the banned word is present in the message
         int flag = 0;
-        if (messages[i].warnings != -1 && strstr(messages[i].message, bannedWord) != NULL) {
-            messages[i].warnings++;
+        if (strstr(messages[i].message, bannedWord) != NULL) {
+            changeValue(map, messages[i].name, (getValue(map, messages[i].name) + 1));
             flag = 1;
-            if (messages[i].warnings >= 3) {
-                messages[i].warnings = -1;
+            if (getValue(map, messages[i].name) >= 3) {
                 printf("%s is banned.\n", messages[i].name);
-                flag = 1;
                 continue;  // Skip printing the message
             }
         }
-        if (flag!=0||messages[i].warnings != -1) {
-            printf("%s: %s\n", "", messages[i].message);
+        if (flag == 0 && getValue(map, messages[i].name) < 3) {
+            printf("%s: %s\n", messages[i].name, messages[i].message);
         }
     }
 
+
+
     return 0;
 }
+
+
 
 
